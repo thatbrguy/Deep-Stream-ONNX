581aefc9eb8cd69c334b123acccd1e8a1770a42a
thatbrguy <bharathrajn98@gmail.com>
2019-11-11 22:24:19 +0530

    minor code cleanup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Deep-Stream-ONNX
+# Deep-Stream-ONNX
 
 How to deploy ONNX models using DeepStream on Jetson Nano. [[Blog](#)] [[Performance](#)]
 
--- custom_bbox_parser/nvdsparsebbox_tiny_yolo.cpp
+++ custom_bbox_parser/nvdsparsebbox_tiny_yolo.cpp
@@ -13,10 +13,10 @@
  * C-linkage [extern "C"] was written to prevent name-mangling. This function must return true after
  * adding all bounding boxes to the objectList vector.
  * 
- * @param outputLayersInfo Vector of NvDsInferLayerInfo objects with information about the output layer.
- * @param networkInfo NvDsInferNetworkInfo object with information about the TinyYOLOv2 network.
- * @param detectionParams NvDsInferParseDetectionParams with information about some config params.
- * @param objectList Vector of NvDsInferParseObjectInfo objects to which bounding box information must
+ * @param [outputLayersInfo] std::vector of NvDsInferLayerInfo objects with information about the output layer.
+ * @param [networkInfo] NvDsInferNetworkInfo object with information about the TinyYOLOv2 network.
+ * @param [detectionParams] NvDsInferParseDetectionParams with information about some config params.
+ * @param [objectList] std::vector of NvDsInferParseObjectInfo objects to which bounding box information must
  * be stored.
  * 
  * @return true
@@ -38,9 +38,9 @@ extern "C" bool NvDsInferParseCustomYoloV2Tiny(
  * negative, it is set to 0. If bbox.left is greater than netW, it is set 
  * to netW.
  * 
- * @param val Value to be bound.
- * @param minVal Lower bound.
- * @param maxVal Upper bound.
+ * @param [val] Value to be bound.
+ * @param [minVal] Lower bound.
+ * @param [maxVal] Upper bound.
  * 
  * @return A value that is bound in the range [minVal, maxVal].
  */
@@ -56,10 +56,10 @@ static unsigned clamp(const uint val, const uint minVal, const uint maxVal)
  * Overlap is computed along either the X or Y axis as specified by the
  * user. If no overlap is available, 0 is returned.
  * 
- * @param x1min Minimum coordinate of the 1st line along a single axis.
- * @param x1max Maximum coordinate of the 1st line along a single axis.
- * @param x2min Minimum coordinate of the 2nd line along a single axis.
- * @param x2max Maximum coordinate of the 2nd line along a single axis.
+ * @param [x1min] Minimum coordinate of the 1st line along a single axis.
+ * @param [x1max] Maximum coordinate of the 1st line along a single axis.
+ * @param [x2min] Minimum coordinate of the 2nd line along a single axis.
+ * @param [x2max] Maximum coordinate of the 2nd line along a single axis.
  * 
  * @return Overlap between the two lines as a float value.
  */
@@ -83,8 +83,8 @@ static float overlap1D(float x1min, float x1max, float x2min, float x2max)
  * the union. IoU is just the ratio of the intersection to the union. A 0 is returned
  * if there is union is 0.
  * 
- * @param bbox1 NvDsInferParseObjectInfo object containing the 1st bounding box info.
- * @param bbox2 NvDsInferParseObjectInfo object containing the 2nd bounding box info.
+ * @param [bbox1] NvDsInferParseObjectInfo object containing the 1st bounding box info.
+ * @param [bbox2] NvDsInferParseObjectInfo object containing the 2nd bounding box info.
  * 
  * @return IoU between the two bounding boxes as a float value.
  */
@@ -108,8 +108,8 @@ static float computeIoU(const NvDsInferParseObjectInfo& bbox1, const NvDsInferPa
  * and return true if the 1st bounding box confidence value is greater than 2nd bounding box 
  * confidence value. This function is passed as an argument for the sorting function.
  * 
- * @param bbox1 NvDsInferParseObjectInfo object containing the 1st bounding box info.
- * @param bbox2 NvDsInferParseObjectInfo object containing the 2nd bounding box info.
+ * @param [bbox1] NvDsInferParseObjectInfo object containing the 1st bounding box info.
+ * @param [bbox2] NvDsInferParseObjectInfo object containing the 2nd bounding box info.
  * 
  * @return Boolean value indicating whether confidence of bbox1 is greater than confidence of bbox2.
  */
@@ -121,13 +121,13 @@ static bool compareBBoxConfidence(const NvDsInferParseObjectInfo& bbox1, const N
 /**
  * Creates the NvDsInferParseObjectInfo Bounding Box object given attributes.
  * 
- * @param bx Bounding Box center X-Coordinate.
- * @param by Bounding Box center Y-Coordinate.
- * @param bw Bounding Box width.
- * @param bh Bounding Box height.
- * @param stride Ratio of the image width to the grid size.
- * @param netW Width of the image.
- * @param netH Height of the image.
+ * @param [bx] Bounding Box center X-Coordinate.
+ * @param [by] Bounding Box center Y-Coordinate.
+ * @param [bw] Bounding Box width.
+ * @param [bh] Bounding Box height.
+ * @param [stride] Ratio of the image width to the grid size.
+ * @param [netW] Width of the image.
+ * @param [netH] Height of the image.
  * 
  * @return NvDsInferParseObjectInfo Bounding Box object.
  */
@@ -161,16 +161,16 @@ static NvDsInferParseObjectInfo createBBox(const float& bx, const float& by, con
  * This function is used to accumulate all the bounding boxes present in a single
  * frame of the video into a vector called bboxInfo.
  * 
- * @param bx Bounding Box center X-Coordinate.
- * @param by Bounding Box center Y-Coordinate.
- * @param bw Bounding Box width.
- * @param bh Bounding Box height.
- * @param stride Ratio of the image width to the grid size.
- * @param netW Width of the image.
- * @param netH Height of the image.
- * @param maxIndex Class ID of the detected bounding box.
- * @param maxProb Confidence of the detected bounding box
- * @param bboxInfo Vector of bounding boxes.
+ * @param [bx] Bounding Box center X-Coordinate.
+ * @param [by] Bounding Box center Y-Coordinate.
+ * @param [bw] Bounding Box width.
+ * @param [bh] Bounding Box height.
+ * @param [stride] Ratio of the image width to the grid size.
+ * @param [netW] Width of the image.
+ * @param [netH] Height of the image.
+ * @param [maxIndex] Class ID of the detected bounding box.
+ * @param [maxProb] Confidence of the detected bounding box
+ * @param [bboxInfo] std::vector of bounding boxes.
  */
 static void addBBoxProposal(const float bx, const float by, const float bw, const float bh,
                      const uint stride, const uint& netW, const uint& netH, const int maxIndex,
@@ -192,10 +192,10 @@ static void addBBoxProposal(const float bx, const float by, const float bw, cons
  * and only retain the bounding box of the highest confidence of that object. Two bounding
  * boxes are considered to belong to the same object if they have a large overlap.
  * 
- * @param inputBBoxInfo Vector of bounding boxes belonging to a single class.
- * @param nmsThresh Overlap threshold for NMS.
+ * @param [inputBBoxInfo] std::vector of bounding boxes belonging to a single class.
+ * @param [nmsThresh] Overlap threshold for NMS.
  * 
- * @return Vector of bounding boxes of a single class after NMS is applied.
+ * @return std::vector of bounding boxes of a single class after NMS is applied.
  */
 static std::vector<NvDsInferParseObjectInfo>
 nonMaximumSuppression(std::vector<NvDsInferParseObjectInfo> inputBBoxInfo, const float nmsThresh)
@@ -226,11 +226,11 @@ nonMaximumSuppression(std::vector<NvDsInferParseObjectInfo> inputBBoxInfo, const
  * 
  * Interatively performs NMS over all classes using the single class NMS function.
  * 
- * @param bboxInfo [description]
- * @param numClasses Number of classes.
- * @param nmsThresh Overlap threshold for NMS.
+ * @param [bboxInfo] [description]
+ * @param [numClasses] Number of classes.
+ * @param [nmsThresh] Overlap threshold for NMS.
  * 
- * @return Vector of bounding boxes of all classes after NMS is applied.
+ * @return std::vector of bounding boxes of all classes after NMS is applied.
  */
 
 static std::vector<NvDsInferParseObjectInfo>
@@ -238,7 +238,7 @@ nmsAllClasses(std::vector<NvDsInferParseObjectInfo>& bboxInfo, const uint numCla
 {
     std::vector<NvDsInferParseObjectInfo> resultBBoxes;
     
-    // Vector of Vector (of size numClasses) to hold classwise bounding boxes.
+    // std::vector of std::vector (of size numClasses) to hold classwise bounding boxes.
     std::vector<std::vector<NvDsInferParseObjectInfo>> splitBoxes(numClasses);
 
     // Bounding box with attribute "classID" is pushed into the index "classID" of "splitBoxes". This
@@ -264,18 +264,18 @@ nmsAllClasses(std::vector<NvDsInferParseObjectInfo>& bboxInfo, const uint numCla
  * Loops over the output of TinyYOLOv2 and calculates the bounding box locations. Ignores bounding
  * boxes with detection probability less than the probability threshold.
  * 
- * @param detections Output of TinyYOLOv2 as a flattened object.
- * @param netW Width of the image.
- * @param netH Height of the image.
- * @param anchors XY locations of the Anchor Boxes multiplied by the stride value provided as a vector.
+ * @param [detections] Output of TinyYOLOv2 as a flattened object.
+ * @param [netW] Width of the image.
+ * @param [netH] Height of the image.
+ * @param [anchors] XY locations of the Anchor Boxes multiplied by the stride value provided as a vector.
  * The anchor box vector is of the form [x_1, y_1, x_2, y_2, ..., x_5, y_5].
- * @param numBBoxes Number of Anchors. This is equal to the number of predicted bounding boxes per grid cell.
- * @param gridSize Size of the grid.
- * @param stride Ratio of the image width to the grid size.
- * @param probThresh Bounding box probability threshold.
- * @param numOutputClasses Overlap threshold for NMS.
+ * @param [numBBoxes] Number of Anchors. This is equal to the number of predicted bounding boxes per grid cell.
+ * @param [gridSize] Size of the grid.
+ * @param [stride] Ratio of the image width to the grid size.
+ * @param [probThresh] Bounding box probability threshold.
+ * @param [numOutputClasses] Overlap threshold for NMS.
  * 
- * @return Vector of bounding boxes of all classes obtained by decoding the TinyYOLOv2 output.
+ * @return std::vector of bounding boxes of all classes obtained by decoding the TinyYOLOv2 output.
  */
 
 static std::vector<NvDsInferParseObjectInfo>
@@ -350,10 +350,10 @@ decodeYoloV2Tensor(
  * C-linkage [extern "C"] was written to prevent name-mangling. This function must return true after
  * adding all bounding boxes to the objectList vector.
  * 
- * @param outputLayersInfo Vector of NvDsInferLayerInfo objects with information about the output layer.
- * @param networkInfo NvDsInferNetworkInfo object with information about the TinyYOLOv2 network.
- * @param detectionParams NvDsInferParseDetectionParams with information about some config params.
- * @param objectList Vector of NvDsInferParseObjectInfo objects to which bounding box information must
+ * @param [outputLayersInfo] std::vector of NvDsInferLayerInfo objects with information about the output layer.
+ * @param [networkInfo] NvDsInferNetworkInfo object with information about the TinyYOLOv2 network.
+ * @param [detectionParams] NvDsInferParseDetectionParams with information about some config params.
+ * @param [objectList] std::vector of NvDsInferParseObjectInfo objects to which bounding box information must
  * be stored.
  * 
  * @return true
@@ -413,8 +413,6 @@ extern "C" bool NvDsInferParseCustomYoloV2Tiny(
     const NvDsInferLayerInfo &layer = outputLayersInfo[0];
     assert (layer.dims.numDims == 3);
 	
-    // std :: cout << layer.dims.d[0] << layer.dims.d[1] << layer.dims.d[2] << "\n";
-
     // Decoding the output tensor of TinyYOLOv2 to the NvDsInferParseObjectInfo format.
     std::vector<NvDsInferParseObjectInfo> objects =
         decodeYoloV2Tensor(
